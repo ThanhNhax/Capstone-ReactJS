296a1b4baeddc9557e0f1ddf894dfaf88796b811
THANH NHA <thanhnha.k13@gamil.com>
2023-04-14 00:18:15 +0700

    css Card

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,25 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 export default function Product({ product }) {
-  const renderFeature = () => {
-    const value = product?.feature;
-    if (value === true) {
-      return <i className=" fa  fa-heart feature_like"></i>;
-    }
-    return <i className=" fa  fa-heart feature"></i>;
-  };
-  return (
-    <div className="card">
-      <img src={product.image} alt="..." />
-      {renderFeature()}
-      <div className="card-body">
-        <h3>{product.name}</h3>
-        <p>{product.shortDescription}</p>
-        <div>
-          <NavLink to={`/detail/${product.id}`}>Buy now</NavLink>
-          <p>{product.price}$</p>
+    return (
+        <div className="card">
+            <div className="card-img">
+                <img src={product.image} alt="..." />
+            </div>
+            <div className="card-body">
+                <h3>{product.name}</h3>
+                <p>{product.shortDescription}</p>
+                <NavLink to={`/detail/${product.id}`}>Buy now</NavLink>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/hoc/ResponsiveItem.jsx
+++ src/hoc/ResponsiveItem.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect, useState } from "react";
 
 export default function ResponsiveItem(props) {
-  const [screen, setScreen] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
-  });
-  useEffect(() => {
-    const handleOnResize = () => {
-      setScreen({
-        width: window.innerWidth,
+    const [screen, setScreen] = useState({
+        width: window.innerWidth, // lấy giá trị hiện
         height: window.innerHeight,
-      });
-    };
-    window.onresize = handleOnResize;
-    return () => {
-      window.removeEventListener("resize", handleOnResize);
-    };
-  }, []);
-  if (screen.width <= 768 && props.componentMobile) {
-    // Load component mobile
-    return <props.componentMobile />;
-  }
-  //Ngược lại thì load component thường
-  return <props.component />;
+    });
+    useEffect(() => {
+        const handleOnResize = () => {
+            setScreen({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        };
+        window.onresize = handleOnResize;
+        return () => {
+            window.removeEventListener("resize", handleOnResize);
+        };
+    }, []);
+    if (screen.width <= 768 && props.componentMobile) {
+        // Load component mobile
+        return <props.componentMobile />;
+    }
+    //Ngược lại thì load component thường
+    return <props.component />;
 }
